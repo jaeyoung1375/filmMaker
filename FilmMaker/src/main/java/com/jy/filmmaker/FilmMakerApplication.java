7a1a2f9a02c43b45154ef0267e1db839873f1f34
jaeyoung1375 <79030661+jaeyoung1375@users.noreply.github.com>
2023-06-16 17:29:43 +0900

    프로젝트 기본세팅 구성

---
+++ FilmMaker/src/main/java/com/jy/filmmaker/FilmMakerApplication.java
@@ -0,0 +1,13 @@
+package com.jy.filmmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmMakerApplication.class, args);
+	}
+
+}
---
+++ FilmMaker/src/test/java/com/jy/filmmaker/FilmMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.filmmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
